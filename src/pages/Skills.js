7ff5475dc9a3e6ac7ae08d4dbd7f867455e5dc59
seonoopy <seonoopy94@gmail.com>
2024-10-13 21:43:23 +0900

    skills

--- src/pages/Skills.js
+++ src/pages/Skills.js
@@ -4,9 +4,13 @@ function Skills() {
   return (
     <div>
       <h1 className="text-6xl md:text-8xl font-bold text-white">Skills</h1>
-      <span className="bg-slate-500 p-2 rounded">skills</span>
+      <span className="bg-slate-500 p-2 rounded">{skills}</span>
     </div>
   );
 }
 
 export default Skills;
+
+const skills = {
+  Language: ["Python", "JavaScript", "Java"],
+};
